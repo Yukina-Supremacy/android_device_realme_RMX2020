fed525a71ecd2946ad324116e77a7a34e40999b3
AgentFabulous <kshitijgm@gmail.com>
2022-03-07 14:54:47 +0000

    RMX2020: Bring back custom light service
    Signed-off-by: Mikazuuu07 <irfn4566@gmail.com>

--- device.mk
+++ device.mk
@@ -117,7 +117,7 @@ PRODUCT_BOOT_JARS += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.RMX2020
+    android.hardware.light@2.0-service.rmx2020
 
 # NFC
 PRODUCT_PACKAGES += \
